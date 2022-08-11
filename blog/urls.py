2695774912de8afc09408a0ce4cdbf7736978f2d
risa1796 <risa1796@gmail.com>
2022-08-11 12:42:55 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
+    path('create_post/', views.PostCreate.as_view()),
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('',views.PostList.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post, Category, Tag
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostList(ListView):
     model = Post
@@ -39,3 +40,31 @@ def category_page(request, slug):
             'category': category,
         }
     )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    ) 
+
+class PostCreate(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content','image','upload_file','category']
+
+    def form_valid(self,form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+
+        else:
+            return redirect('/blog/')
